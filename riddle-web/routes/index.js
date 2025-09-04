e8651d5fb4fa9e862f2c013cde88318cbef5fbe3
Vyctor Huggo <74301628+Vyctor-Huggo@users.noreply.github.com>
2025-09-03 23:55:16 -0300

    tá biito

--- riddle-web/routes/index.js
+++ riddle-web/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/1', (req, res) => {
+router.get('/01', (req, res) => {
   res.render('index', {
     title: "O Primeiro",
     mainText: "1 wxyz tuv ghi",
@@ -11,12 +11,12 @@ router.get('/1', (req, res) => {
     footerText: "© 2025 Riddle App",
     response: "na pior em paris e londres",
     tip: "dica:",
-    nextPage: "/riddle/2"
+    nextPage: "/riddle/1"
 
   });
 });
 
-router.get('/2', (req, res) => {
+router.get('/1', (req, res) => {
   res.render('index', {
     title: "Onde esta o coelho?",
     mainText: "<div> Título </div>",
@@ -25,25 +25,26 @@ router.get('/2', (req, res) => {
     footerText: "© 2025 Riddle App",
     response: "desfigurado",
     tip: "dica: é como magica",
+    nextPage: "/riddle/2"
+
+  });
+});
+
+router.get('/2', (req, res) => {
+  res.render('index', {
+    title: "Samuel Ratchett",
+    mainText: "o que importa e que voce esta bem nem que seja, por poucos segundos antes que o mundo, caia sobre você",
+    altMainImg: "Oi, tudo bem?",
+    mainImage: "/images/1986.png",
+    footerText: "© 2025 Riddle App",
+    response: "na pior em paris e londres",
+    tip: "dica:",
     nextPage: "/riddle/3"
 
   });
 });
-
+  
 router.get('/3', (req, res) => {
-  res.render('index', {
-    title: "O Primeiro",
-    mainText: "1 wxyz tuv ghi",
-    mainImage: "/images/g-o.svg",
-    footerText: "© 2025 Riddle App",
-    response: "na pior em paris e londres",
-    tip: "dica:",
-    nextPage: "/riddle/4"
-
-  });
-});
-
-router.get('/4', (req, res) => {
   res.render('index', {
     title: "O Primeiro",
     mainText: "1 wxyz tuv ghi",
@@ -55,5 +56,7 @@ router.get('/4', (req, res) => {
 
   });
 });
+
+//fibonacci até o 7
 
 module.exports = router;
